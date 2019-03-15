f0dae1d75cc48912d576f06ee06c8c9b76d8c486
ZhouZhuoFei <fregot@icloud.com>
2019-03-15 23:31:20 +0800

    +

--- Game/first.swift
+++ Game/first.swift
@@ -10,23 +10,60 @@ import UIKit
 
 class first: UIViewController {
 
-    @IBOutlet weak var welcome: first_2!
-    @IBOutlet weak var user: UITextField!
+
+
     var textField: UITextField?
+    var password: UITextField?
+    @IBOutlet weak var signin: UIButton!
+    @IBAction func sign(_ sender: Any) {
+        print("hello")
+    }
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         let mylabel: UILabel = UILabel()
-        mylabel.frame = CGRect(x: 0, y: 0, width: 185, height: 30)
-        mylabel.layer.position = CGPoint(x: self.view.bounds.width/2, y: 200)
+        mylabel.frame = CGRect(x: 0, y: 0, width: 300, height: 100)
+        mylabel.layer.position = CGPoint(x: self.view.bounds.width/2, y: 150)
         mylabel.textColor = UIColor.white
         mylabel.backgroundColor = UIColor.black
         mylabel.text = "Tell something to world!"
+        mylabel.textAlignment = NSTextAlignment.center
+        mylabel.layer.masksToBounds = true
+        mylabel.layer.cornerRadius = 20.0
         self.view.addSubview(mylabel)
         textField = UITextField(frame: CGRect(x: 0, y: 0, width:185, height: 30 ))
         textField?.layer.position = CGPoint(x: self.view.bounds.width/2, y: 250)
-        textField?.borderStyle = UITextField.BorderStyle.line
+        textField?.borderStyle = UITextField.BorderStyle.roundedRect
         textField?.placeholder = "Username"
         self.view.addSubview(textField!)
+        password = UITextField(frame: CGRect(x: 0, y: 0, width: 185, height: 30))
+        password?.layer.position = CGPoint(x: self.view.bounds.width/2, y: 300)
+        password?.borderStyle = UITextField.BorderStyle.roundedRect
+        password?.placeholder = "password"
+        self.view.addSubview(password!)
+        signin.layer.position = CGPoint(x: 250, y: 340)
+        signin.setTitleColor(UIColor.white, for: .normal)
+        signin.backgroundColor = UIColor(displayP3Red: 0.0, green: 0.5, blue: 0.5, alpha: 0.5)
+        
+        signin.layer.cornerRadius = 10
+        signin.layer.borderWidth = 1
+
+        
+        
+        /*let button = UIButton()
+        button.setTitle("Sign in", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.frame = CGRect(x: 0, y: 0, width: 60, height: 30)
+        button.tag = 1
+        button.layer.position = CGPoint(x: 240, y: 350)
+        button.backgroundColor = UIColor(red: 0.0, green: 0.2, blue: 1, alpha: 1)
+        button.layer.cornerRadius = 10
+        button.layer.borderWidth = 1
+        button.addTarget(self, action: "tapped:", for: .touchUpInside)
+        
+        self.view.addSubview(button)*/
+
         
         
 
